989a2ddf68e2431adc234097b8a4bb02c8b0a524
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-01-20 22:08:08 +0200

    Added some descriptions to methods Refactor some sql queries Added search by name method Made code a bit normally

--- bot_functions.py
+++ bot_functions.py
@@ -5,8 +5,9 @@ All things bot can do are here
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
+
 class AnimeBot:
-    def __init__(self, database = 'sqlite:///animeTgBot.db'):
+    def __init__(self, database='sqlite:///animeTgBot.db'):
         """
 
         :param database: link on database(sqlite:///animeTgBot.db in my case)
@@ -23,61 +24,114 @@ class AnimeBot:
         """
 
         sql_content_query = text(f'SELECT * FROM \'{media}\''
-                               f'ORDER BY RANDOM() LIMIT 1')     # sql query to choose random film or serial
+                                 f'ORDER BY RANDOM() LIMIT 1')  # sql query to choose random film or serial
 
         content = self.connection.execute(sql_content_query).fetchone()
+        content_dict = content_to_dict(content)
 
-        return content
+        return content_dict
 
     def choose_top10(self, media):
         """
         This function choose top 10 serials or films
+
         :param media: table name in sql database(SERIALS or FILMS in my case)
         :return: list of sets
         """
-        sql_top10_text = text(f'SELECT * FROM {media} ' \
-                              f'ORDER BY rating DESC')
-        top10_list = self.connection.execute(sql_top10_text).fetchmany(10)
-        return top10_list
+        sql_top10_query = text(f'SELECT * FROM \'{media}\''
+                               f'ORDER BY rating DESC LIMIT 10')  # sql query that give you top 10 serials/films from media
+
+        top10_content_list = self.connection.execute(sql_top10_query).fetchall()
+        content_list = []
+
+        for content_set in top10_content_list:
+            content_dict = content_to_dict(content_set)
+            content_list.append(content_dict)
+
+        return content_list
 
     def choose_random_by_genre(self, media, genre='Комедии'):
         """
         Same as choose_random, but you can also enter your genre
         """
-        from random import choice as random_choice
 
-        sql_max_text = text(f'SELECT * FROM {media} '
-                            f'WHERE genre = \'{genre}\'')
+        sql_random_query = text(f'SELECT * FROM \'{media}\''
+                                f'WHERE genre = \'{genre}\''
+                                f'ORDER BY RANDOM() LIMIT 1')
 
-        random_list = self.connection.execute(sql_max_text).fetchall()
-        serial = random_choice(random_list)
+        content = self.connection.execute(sql_random_query).fetchone()
+        content_dict = content_to_dict(content)
 
-        return serial
+        return content_dict
 
     def choose_top10_by_genre(self, media, genre='Драма'):
         """
         Save as choose_top10, but you can also enter your genre
         """
-        sql_max_text = text(f'SELECT * FROM {media} '
-                            f'WHERE genre = \'{genre}\''
-                            f'ORDER BY rating DESC')
+        sql_top_10_query = text(f'SELECT * FROM {media} '
+                                f'WHERE genre = \'{genre}\''
+                                f'ORDER BY rating DESC LIMIT 10')
 
-        rating_list = self.connection.execute(sql_max_text).fetchmany(10)
-        return rating_list
+        top10_content_list = self.connection.execute(sql_top_10_query).fetchall()
+        content_list = []
 
-    def find_by_name(self, media, name):
+        for content_set in top10_content_list:
+            content_dict = content_to_dict(content_set)
+            content_list.append(content_dict)
+
+        return content_list
+
+    def find_by_name(self, media, name_to_find):
         """
-        Finding film or serial, which title is close to given parametr name
+        Finding film or serial, which title is close to given name
+
         :param name: Name of anime, string
+        :return serials: serials whith names like in name_query
         """
-        sql_find_text = text(f'SELECT * FROM {media} '
-                             f'WHERE UPPER(name) LIKE UPPER(\'%{name}%\')')
+        sql_find_query = text(f'SELECT * FROM {media} '
+                              f'WHERE UPPER(name) LIKE UPPER(\'%{name_to_find}%\')')
 
-        serial = self.connection.execute(sql_find_text).fetchall()
-        return serial
+        content = self.connection.execute(sql_find_query).fetchall()
+        if content:
+            content_list = []
+            for anime in content:
+                content_dict = content_to_dict(anime)
+                content_list.append(content_dict)
+            return content_list
+        return 'Извини, не смог найти ничего похожего'
 
     def select_ongoins(self):
-        sql_find_text = text(f'SELECT * FROM SERIALS '
-                             f'WHERE genre is \'Текущие сезоны (Онгоинги)\'')
-        serials = self.connection.execute(sql_find_text).fetchall()
-        return serials
+        """
+        Selecting ongoings
+        :return: ongoings serials
+        """
+        sql_ongiongs_query = text(f'SELECT * FROM SERIALS '
+                                  f'WHERE genre is \'Текущие сезоны (Онгоинги)\'')
+
+        serial = self.connection.execute(sql_ongiongs_query).fetchall()
+        serials_list = []
+        for ser in serial:
+            serials_list.append(content_to_dict(ser))
+
+        return serials_list
+
+
+def content_to_dict(content: set) -> dict:
+    # From set to dict
+    content_dict = {'name': content[1],
+                    'link': content[2],
+                    'genre': content[3],
+                    'rating': content[4],
+                    'description': content[5]}
+
+    return content_dict
+
+
+def content_to_html(content: dict) -> str:
+    html_text = f'<a href=\'{content["link"]}\'>{content["name"]}</a>\n' \
+                f'⭐{content["rating"]}\n' \
+                f'📄{content["genre"]}\n' \
+                f'✍{content["description"][0:350] + "..."}\n'
+
+    return html_text
+
--- main.py
+++ main.py
@@ -1,36 +1,53 @@
-import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 import keyboards
-from bot_functions import AnimeBot
+from bot_functions import AnimeBot, content_to_html
 
 TOKEN = '1410462743:AAHmabWssHSalBRNIQhQeGpFRqGX6aw7jWo'
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 BOT_FUNCTIONS = AnimeBot()
 
+
 class IsSerial:
+    # Check user choice - Serials or Films
     bol = True
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message, text = 'Привет \nЯ телеграм бот, который поможет тебе с выбором аниме'):
-    await bot.send_message(chat_id=message.from_user.id, \
-                           text= text, \
-                           reply_markup=keyboards.StartKeyboard().keyboard)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message,
+                       message_text='Привет \n' \
+                                    'Я телеграм бот, который поможет тебе с выбором аниме))\n\n' \
+                                    'Если нужна будет помощь, напиши /help'):
+    """
+    Welcome function
+    :param message_text: This text will be in bot`s message
+    :param message: message like in commands
+    """
+    chat_id = message.from_user.id
+    await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=keyboards.StartKeyboard.keyboard)
 
 
 @dp.message_handler(lambda message: message.text == f'⬅ Назад')
 async def back_to_start(message):
-    await send_welcome(message, text='Хорошо')
-
+    """
+    Returns user to the start keyboard
+    """
+    message_text = 'Хорошо'
+    await send_welcome(message, message_text)
 
 
 @dp.message_handler(lambda message: message.text in ['Аниме сериалы', 'Аниме фильмы'])
-async def anime_choose(message):
-    bot_message = 'Хорошо, но что именно ты хочешь?'
+async def choose_media(message):
+    """
+    Choosing media function
+    :param message:
+    :return:
+    """
+    message_text = 'Хорошо, но что именно ты хочешь?'
+    chat_id = message.from_user.id
+
     if message.text == 'Аниме сериалы':
         IsSerial.bol = True
         reply_markup = keyboards.SerialsKeyboard.keyboard
@@ -38,38 +55,49 @@ async def anime_choose(message):
         IsSerial.bol = False
         reply_markup = keyboards.FilmsKeyboard.keyboard
 
-    await bot.send_message(chat_id=message.from_user.id, text=bot_message, \
-                           reply_markup=reply_markup)
-
+    await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=reply_markup)
 
 
 @dp.message_handler(lambda message: message.text in ['Случайный фильм', 'Случайный сериал'])
 async def random_anime(message):
-    if message.text == 'Случайный сериал':
-        link = BOT_FUNCTIONS.choose_random('SERIALS')[2]
-    else:
-        link = BOT_FUNCTIONS.choose_random('FILMS')[2]
+    # Send random anime to user
+    chat_id = message.from_user.id
 
-    await bot.send_message(chat_id=message.from_user.id, text=link)
+    if message.text == 'Случайный сериал':
+        content = BOT_FUNCTIONS.choose_random('SERIALS')
+    else:
+        content = BOT_FUNCTIONS.choose_random('FILMS')
+
+    message_text = content_to_html(content)
+
+    await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='html', disable_web_page_preview=True)
 
 
 @dp.message_handler(lambda message: message.text in ['Сериалы по жанрам', 'Фильмы по жанрам'])
-async def serials_genre(message):
+async def media_genres_keyboard(message):
+    # Reply genre keyboard
+
+    message_text = 'Отлично, какой жанр ты предпочитаешь?'
+    chat_id = message.from_user.id
+
     if message.text == 'Сериалы по жанрам':
-        reply = keyboards.SerialsGenresKeyboard.keyboard
+        reply_keyboard = keyboards.SerialsGenresKeyboard.keyboard
     else:
-        reply = keyboards.FilmsGenresKeyboard.keyboard
-    await bot.send_message(chat_id=message.from_user.id,\
-                           text='Отлично, какой жанр ты предпочитаешь?', \
-                           reply_markup=reply)
+        reply_keyboard = keyboards.FilmsGenresKeyboard.keyboard
+
+    await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=reply_keyboard)
+
 
 @dp.message_handler(lambda message: message.text in ['Назад к аниме сериалам', 'Назад к аниме фильмам'])
-async def back_to_serials(message):
+async def back_to_media_keyboard(message):
+    chat_id = message.from_user.id
+    message_text = '👇Вернулись, что дальше?👇'
     if message.text == 'Назад к аниме сериалам':
         reply_markup = keyboards.SerialsKeyboard.keyboard
     else:
         reply_markup = keyboards.FilmsKeyboard.keyboard
-    await bot.send_message(message.from_user.id, text='Ok', reply_markup=reply_markup)
+
+    await bot.send_message(chat_id=chat_id, text=message_text, reply_markup=reply_markup)
 
 
 @dp.message_handler(lambda message: message.text in ['Боевые искусства', 'Война', 'Детектив', 'Драма', 'Сёдзё',
@@ -78,30 +106,68 @@ async def back_to_serials(message):
                                                      'Романтика', 'Комедии', 'Спорт', 'Приключения',
                                                      'Триллер', 'Ужасы', 'Школа'])
 async def random_genre(message):
-    if IsSerial.bol == True:
-        link = BOT_FUNCTIONS.choose_random_by_genre(media='SERIALS', genre=message.text)[2]
+    # Reply random serial, but user choose genre
+    chat_id = message.from_user.id
+    if IsSerial.bol:
+        content = BOT_FUNCTIONS.choose_random_by_genre(media='SERIALS', genre=message.text)
     else:
-        link = BOT_FUNCTIONS.choose_random_by_genre(media='FILMS', genre=message.text)[2]
-    await bot.send_message(message.from_user.id, text=link)
+        content = BOT_FUNCTIONS.choose_random_by_genre(media='FILMS', genre=message.text)
+
+    message_text = '👇Вот случайный тайтл этого жанра👇\n'
+    message_text += content_to_html(content)
+
+    await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='html', disable_web_page_preview=True)
 
 
 @dp.message_handler(lambda message: message.text in ['Топ фильмов', 'Топ сериалов'])
 async def top_media(message):
+    # Need refactoring
+    chat_id = message.from_user.id
+
     if message.text == 'Топ фильмов':
-        links = BOT_FUNCTIONS.choose_top10('FILMS')
+        content = BOT_FUNCTIONS.choose_top10('FILMS')
     else:
-        links = BOT_FUNCTIONS.choose_top10('SERIALS')
-    text = ""
-    for anime in links:
-        text += f"<a href=\'{anime[2]}\'>{anime[1]}</a>\n"
-    await bot.send_message(text=text, chat_id=message.from_user.id, parse_mode="html", disable_web_page_preview=True)
+        content = BOT_FUNCTIONS.choose_top10('SERIALS')
+
+    message_text = ''
+    for anime in content:
+        message_text += f'<a href=\'{anime["link"]}\'>{anime["name"]}</a>\n'
+
+    await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='html', disable_web_page_preview=True)
+
 
 @dp.message_handler(lambda message: message.text == 'Онгоинги')
 async def ongoins(message):
-    links =BOT_FUNCTIONS.select_ongoins()
-    text = ""
-    for anime in links:
-        text += f"<a href=\'{anime[2]}\'>{anime[1]}</a>\n"
-    await bot.send_message(text=text, chat_id=message.from_user.id, parse_mode="html", disable_web_page_preview=True)
+    # Need refactoring
+    chat_id = message.from_user.id
+    content = BOT_FUNCTIONS.select_ongoins()
+    message_text = ''
+    for anime in content:
+        message_text += f'<a href=\'{anime["link"]}\'>{anime["name"]}</a>\n'
+    await bot.send_message(chat_id=chat_id ,text=message_text, parse_mode='html', disable_web_page_preview=True)
+
+@dp.message_handler()
+async def other_text_to_search(message):
+    #Need refactoring
+
+    chat_id = message.from_user.id
+
+    if IsSerial:
+        media = 'SERIALS'
+    else:
+        media = 'FILMS'
+
+    content = BOT_FUNCTIONS.find_by_name(media,message.text)
+
+    if isinstance(content, list):
+        message_text = ''
+        for anime in content:
+            message_text += f'<a href=\'{anime["link"]}\'>{anime["name"]}</a>\n'
+    else:
+        message_text = content
+
+    await bot.send_message(chat_id=chat_id, text=message_text, parse_mode='html', disable_web_page_preview=True)
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
