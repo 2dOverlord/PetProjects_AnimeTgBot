194afdf7766666ddb313caa14e78bc8ec5e356bb
Vladyslav Kriuchkovskyi <kriuchkovskyi.vlad@gmail.com>
2021-02-15 17:06:39 +0200

    Added AFISHA table whith information about all announcements

--- creating database/creating_database.py
+++ creating database/creating_database.py
@@ -29,6 +29,15 @@ films_table = Table('FILMS', metadata,
                      Column('description', String),
                      Column('image_url', String))
 
+anons_table = Table('ANONS', metadata,
+                     Column('id', Integer, primary_key=True),
+                     Column('name', String),
+                     Column('link', String),
+                     Column('genre', String),
+                     Column('rating', Integer),
+                     Column('description', String),
+                     Column('image_url', String))
+
 metadata.create_all(engine)
 
 def filling_serials_table():
@@ -67,8 +76,21 @@ def filling_ongoings():
                                         description=serial['description'],
                                         image_url = serial['image'])
 
+def filling_anons_table():
+    insert_serials_stmt = anons_table.insert(bind = engine)
+    for i in range(1, 8):  # How many pages on site
+        link = f'http://baza.animevost.tv/anonsy/page/{i}/'
+        for serial in parse_site_page(link):
+            insert_serials_stmt.execute(name = serial['name'],
+                                        link = serial['link'],
+                                        genre = serial['genre'],
+                                        rating = int(serial['rating']),
+                                        description = serial['description'],
+                                        image_url = serial['image'])
+
 
 if __name__ == '__main__':
-    filling_serials_table()
-    filling_films_table()
-    filling_ongoings()
+    # filling_serials_table()
+    # filling_films_table()
+    # filling_ongoings()
+    filling_anons_table()
